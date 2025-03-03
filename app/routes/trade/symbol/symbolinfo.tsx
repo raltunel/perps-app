a99807f0a508d749584e3fa9a2961129328a4828
Rıdvan Altunel <raltunel@proven.com.tr>
2025-03-03 15:13:45 +0300

    add subscription method into symbol info component

--- app/routes/trade/symbol/symbolinfo.tsx
+++ app/routes/trade/symbol/symbolinfo.tsx
@@ -1,6 +1,8 @@
 import { useTradeDataStore } from '~/stores/TradeDataStore';
 import styles from './symbolinfo.module.css';
 import ComboBox from '~/components/Inputs/ComboBox/ComboBox';
+import { useWsObserver } from '~/hooks/useWsObserver';
+import { useEffect } from 'react';
 
 interface SymbolInfoProps {
 }
@@ -15,15 +17,38 @@ const symbolList = [
   'DOGE',
 ]
 
+
+
 const SymbolInfo: React.FC<SymbolInfoProps> = ({ }) => {
 
 
+  const {subscribe, unsubscribeAllByChannel} = useWsObserver();
+
   const {symbol, setSymbol} = useTradeDataStore();
+
+  
+  useEffect(() => {
+    return () => {
+      unsubscribeAllByChannel('activeAssetCtx');
+    }
+  }, [])
+
+  useEffect(() => {
+    subscribe('activeAssetCtx', {
+      payload: {coin: symbol},
+      handler: (payload) => {
+        console.log('>>>', payload)
+      },
+      single: true
+    })
+  }, [symbol])
+  
   
 
 
   return (
     <div className={styles.symbolInfoContainer}>
+      asdasd
       <ComboBox
         value={symbol}
         options={symbolList}
