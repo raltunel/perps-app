a7dfae993f49465ecb95e39067c62861eb40a301
Rıdvan Altunel <raltunel@proven.com.tr>
2025-03-03 15:26:58 +0300

    add interface and process files for symbol info component

---
+++ app/utils/SymbolInfoIFs.ts
@@ -0,0 +1,13 @@
+export interface SymbolInfoIF {
+    coin: string;
+    dayBaseVlm: number;
+    dayNtlVlm: number;
+    funding:number;
+    impactPxs: number[];
+    markPx: number;
+    midPx: number;
+    openInterest: number;
+    oraclePx: number;
+    premium: number;
+    prevDayPx: number;
+}
