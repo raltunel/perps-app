d86f07d5c8df06d67ecfa5c2831a58e4e5faac90
Jadessu <junioradessu@gmail.com>
2025-03-04 18:13:48 -0600

    to resume

--- app/root.tsx
+++ app/root.tsx
@@ -41,9 +41,7 @@ export default function App() {
           <Outlet />
         </main>
 
-        <footer className='footer'>
-          <p>Footer</p>
-        </footer>
+       
       </div>
     </Layout>
   );
--- app/routes/vaults/vaults.tsx
+++ app/routes/vaults/vaults.tsx
@@ -1,5 +1,14 @@
 import Welcome from '~/components/Welcome/Welcome';
 import type { Route } from '../../+types/root';
+import styles from './vaults.module.css';
+import Button from '~/components/Button/Button';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import { AiOutlineClose } from 'react-icons/ai';
+import VaultTimeframe from '~/components/Vault/VaultTimeframe/VaultTimeframe';
+import { protocolVaults, userVaults } from './data';
+import VaultRow from '~/components/Vault/VaultRow/VaultRow';
+import VaultRowHeader from '~/components/Vault/VaultRowHeader/VaultRowHeader';
 // import styles from './vaults.module.css'
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,5 +22,94 @@ export function loader({ context }: Route.LoaderArgs) {
 }
 
 export default function Vaults({ loaderData }: Route.ComponentProps) {
-  return <Welcome title='Vaults' />;
+  const [searchQuery, setSearchQuery] = useState('');
+  const [activeFilters, setActiveFilters] = useState<Record<string, boolean>>(
+    {}
+  );
+  const [activeTimeframe, setActiveTimeframe] = useState<string>("30D");
+  const [ isTimeframeDropdownOpen, setIsTimeframeDropdownOpen] = useState(false)
+
+  const filters: string[] = ['Leading', 'Deposited', 'Others', 'Closed'];
+
+  const toggleFilter = (filter: string) => {
+    setActiveFilters((prev) => ({
+      ...prev,
+      [filter]: !prev[filter],
+    }));
+  };
+
+  return (
+    <div className={styles.container}>
+      <section className={styles.headerContainer}>
+        <div className={styles.totalValueContainer}>
+          <h3>$69.000,000</h3>
+          <p>
+            Total Value Locked.
+            <a href='/' className={styles.learnMore}>
+              Learn more
+            </a>
+          </p>
+        </div>
+        <Button size='medium' selected>
+          Text
+        </Button>
+      </section>
+
+      <section className={styles.tableContainer}>
+        <header>
+          <div className={styles.searchContainer}>
+            <div className={styles.icon}>
+              <FiSearch size={18} />
+            </div>
+            <input
+              type='text'
+              placeholder='Search by vault address, name, or leader...'
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className={styles.searchInput}
+            />
+            <div className={styles.icon}>
+              {searchQuery && (
+                <AiOutlineClose size={18} onClick={() => setSearchQuery('')} />
+              )}
+            </div>
+          </div>
+
+          <div className={styles.rightSide}>
+            <div className={styles.filterButtonsContainer}>
+              {filters.map((filter) => (
+                <button
+                  key={filter}
+                  className={`${styles.filterButton} ${
+                    activeFilters[filter] ? styles.activeFilterButton : styles.inactiveFilterButton
+                  }`}
+                  onClick={() => toggleFilter(filter)}
+                >
+                  {filter}
+                </button>
+              ))}
+            </div>
+            <VaultTimeframe timeframe={activeTimeframe} setTimeframe={setActiveTimeframe} open={ isTimeframeDropdownOpen} setOpen={setIsTimeframeDropdownOpen} />
+          </div>
+        </header>
+
+        <section className={styles.vaultSectionContainer}>
+          <h3 className={styles.sectionTitle}>Protocol Vaults</h3>
+          <VaultRowHeader/>
+          {protocolVaults.map((vault, idx) => (
+            <VaultRow vault={vault} />
+          ))}
+
+        </section>
+        <section className={styles.vaultSectionContainer}>
+          <h3 className={styles.sectionTitle}>User Vaults</h3>
+          <VaultRowHeader/>
+          {userVaults.map((vault, idx) => (
+            <VaultRow vault={vault} />
+          ))}
+
+        </section>
+      </section>
+    </div>
+  );
 }
