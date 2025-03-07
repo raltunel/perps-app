e2b1c5faebdd1f887a1d3ee66aa0c81cd9a6cb97
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-03-07 12:32:37 -0500

    stub up basic content in `AppOptions.tsx` file

--- app/components/AppOptions/AppOptions.tsx
+++ app/components/AppOptions/AppOptions.tsx
@@ -3,8 +3,25 @@ import styles from './AppOptions.module.css';
 export default function AppOptions() {
     return (
         <section className={styles.app_options}>
-            <h2>App Options!</h2>
-            <p>This is the app options menu.</p>
+            <header>
+                <div />
+                <h2>Options</h2>
+                <button>×</button>
+            </header>
+            <ul>
+                <li>First option here</li>
+                <li>Second option here</li>
+                <li>Third option here</li>
+            </ul>
+            <hr />
+            <ul>
+                <li>Fourth option here</li>
+                <li>Fifth option here</li>
+            </ul>
+            <footer>
+                <button>Cancel</button>
+                <button>Confirm</button>
+            </footer>
         </section>
     );
 }
