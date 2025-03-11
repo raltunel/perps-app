5f06accb4e4dfcd35ac988aebe0bd89661983ad2
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-03-11 16:02:32 -0400

    add explicit type declarations to `AppOptions.tsx` file

--- app/stores/AppOptionsStore.ts
+++ app/stores/AppOptionsStore.ts
@@ -51,14 +51,14 @@ export const useAppOptions = create<useAppOptionsIF>()(
             showBuysSellsOnChart: true,
             showPnL: true,
             showAllWarnings: true,
-            enable: (o: appOptions) => set({[o]: true}),
-            disable: (o: appOptions) => set({[o]: false}),
-            toggle: (o: appOptions) => {
+            enable: (o: appOptions): void => set({[o]: true}),
+            disable: (o: appOptions): void => set({[o]: false}),
+            toggle: (o: appOptions): void => {
                 console.log('updating: ' + o);
                 console.log(typeof o);
                 set({[o]: !get()[o]});
             },
-            multiToggle: (o: appOptions[]) => {
+            multiToggle: (o: appOptions[]): void => {
                 console.log('multi-toggle!');
                 const changes: Partial<Record<appOptions, boolean>> = {};
                 o.forEach((opt: appOptions) => changes[opt] = !get()[opt])
