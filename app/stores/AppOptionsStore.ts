4ee0889b6dcf349acb87b094eb5f59a1fb9894a2
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-03-11 20:15:32 -0400

    simplify types in `AppOptionsStore.ts` file

--- app/stores/AppOptionsStore.ts
+++ app/stores/AppOptionsStore.ts
@@ -1,42 +1,7 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware'
 
-export type appOptions = 
-    | 'skipOpenOrderConfirm'
-    | 'skipClosePositionConfirm'
-    | 'optOutSpotDusting'
-    | 'persistTradingConnection'
-    | 'displayVerboseErrors'
-    | 'enableBackgroundFillNotif'
-    | 'playFillSound'
-    | 'animateOrderBook'
-    | 'clickToSetOrderBookSize'
-    | 'showBuysSellsOnChart'
-    | 'showPnL'
-    | 'showAllWarnings';
-
-export interface useAppOptionsIF {
-    skipOpenOrderConfirm: boolean;
-    skipClosePositionConfirm: boolean;
-    optOutSpotDusting: boolean;
-    persistTradingConnection: boolean;
-    displayVerboseErrors: boolean;
-    enableBackgroundFillNotif: boolean;
-    playFillSound: boolean;
-    animateOrderBook: boolean;
-    clickToSetOrderBookSize: boolean;
-    showBuysSellsOnChart: boolean;
-    showPnL: boolean;
-    showAllWarnings: boolean;
-    enable: (o: appOptions) => void;
-    disable: (o: appOptions) => void;
-    toggle: (o: appOptions|appOptions[]) => void;
-    applyDefaults: () => void;
-}
-
-const LS_KEY = 'APP_OPTIONS';
-
-const DEFAULTS: Record<appOptions, boolean> = {
+const DEFAULTS = {
     skipOpenOrderConfirm: false,
     skipClosePositionConfirm: false,
     optOutSpotDusting: false,
@@ -50,6 +15,17 @@ const DEFAULTS: Record<appOptions, boolean> = {
     showPnL: true,
     showAllWarnings: true,
 }
+
+export type appOptions = keyof typeof DEFAULTS;
+
+export interface useAppOptionsIF extends Record<appOptions, boolean> {
+    enable: (o: appOptions) => void;
+    disable: (o: appOptions) => void;
+    toggle: (o: appOptions|appOptions[]) => void;
+    applyDefaults: () => void;
+}
+
+const LS_KEY = 'APP_OPTIONS';
 
 export const useAppOptions = create<useAppOptionsIF>()(
     persist(
