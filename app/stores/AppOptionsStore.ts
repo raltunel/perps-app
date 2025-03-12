18e239ca711a66af1b0bda171fd67afe70569755
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-03-12 09:42:03 -0400

    annotate code in `AppOptionsStore.ts` file

--- app/stores/AppOptionsStore.ts
+++ app/stores/AppOptionsStore.ts
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
+// default values for the state values managed by this hook
 const DEFAULTS = {
     skipOpenOrderConfirm: false,
     skipClosePositionConfirm: false,
@@ -16,8 +17,10 @@ const DEFAULTS = {
     showAllWarnings: true,
 }
 
+// string-union type of all keys in the `DEFAULTS` obj
 export type appOptions = keyof typeof DEFAULTS;
 
+// extended interface describing the shape of `DEFAULTS` and reducers
 export interface useAppOptionsIF extends Record<appOptions, boolean> {
     enable: (o: appOptions) => void;
     disable: (o: appOptions) => void;
@@ -25,19 +28,29 @@ export interface useAppOptionsIF extends Record<appOptions, boolean> {
     applyDefaults: () => void;
 }
 
+// key to identify data obj in local storage
 const LS_KEY = 'APP_OPTIONS';
 
+// hook to manage global state and local storage
 export const useAppOptions = create<useAppOptionsIF>()(
+    // persist data in local storage (only values, not reducers)
     persist(
         (set, get) => ({
+            // consume data from the `DEFAULT` obj, persisted data from
+            // ... local storage will re-hydrate if present
             ...DEFAULTS,
+            // set a given option to `true`
             enable: (o: appOptions): void => set({[o]: true}),
+            // set a given option to `false`
             disable: (o: appOptions): void => set({[o]: false}),
+            // toggle a value `true` ⟷ `false`
             toggle: (o: appOptions): void => set({[o]: !get()[o]}),
             applyDefaults: (): void => set(DEFAULTS),
         }),
         {
+            // key for local storage
             name: LS_KEY,
+            // format and destination of data
             storage: createJSONStorage(() => localStorage),
         },
     ),
