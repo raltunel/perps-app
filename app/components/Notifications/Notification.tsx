399df5dcb0b3a2ccede1a86188073fe90cee7a8a
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-03-17 11:38:59 -0400

    add code to populate notification data on user click

--- app/components/Notifications/Notification.tsx
+++ app/components/Notifications/Notification.tsx
@@ -1,7 +1,6 @@
 import { IoCheckmarkCircleOutline, IoClose } from 'react-icons/io5';
 import styles from './Notification.module.css';
 import { ImSpinner8 } from 'react-icons/im';
-import Modal from '../Modal/Modal';
 
 type statuses = 'pending'|'complete';
 
@@ -14,24 +13,22 @@ export default function Notification(props: propsIF) {
     const { status, dismiss } = props;
 
     return (
-        <Modal>
-            <section className={styles.notification}>
-                <header>
-                    <div className={styles.header_content}>
-                        {
-                            status === 'pending' &&
-                            <ImSpinner8 size={24} color={'var(--accent1)'} />
-                        }
-                        {
-                            status === 'complete' &&
-                            <IoCheckmarkCircleOutline size={24} color={'var(--accent1)'} />
-                        }
-                        <h2>Leverage Mode Changed</h2>
-                    </div>
-                    <IoClose className={styles.close} onClick={dismiss} />
-                </header>
-                <p>Switched to 100x Leverage</p>
-            </section>
-        </Modal>
+        <section className={styles.notification}>
+            <header>
+                <div className={styles.header_content}>
+                    {
+                        status === 'pending' &&
+                        <ImSpinner8 size={24} color={'var(--accent1)'} />
+                    }
+                    {
+                        status === 'complete' &&
+                        <IoCheckmarkCircleOutline size={24} color={'var(--accent1)'} />
+                    }
+                    <h2>Leverage Mode Changed</h2>
+                </div>
+                <IoClose className={styles.close} onClick={dismiss} />
+            </header>
+            <p>Switched to 100x Leverage</p>
+        </section>
     );
 }
--- app/routes/testpage/testpage.tsx
+++ app/routes/testpage/testpage.tsx
@@ -1,15 +1,19 @@
 import styles from './testpage.module.css';
 import { type useModalIF, useModal } from "~/hooks/useModal";
 import Notification from '~/components/Notifications/Notification';
+import { useEffect, useState } from 'react';
+import Modal from '~/components/Modal/Modal';
 
 interface propsIF {
 
 }
 
+type notificationType = 'spinner'|'check';
+
 interface notificationMetaIF {
     title: string;
     messages: string[],
-    icon: 'spinner'|'check';
+    icon: notificationType;
 }
 
 const notificationMeta: notificationMetaIF[] = [
@@ -28,25 +32,64 @@ const notificationMeta: notificationMetaIF[] = [
         ],
         icon: 'check',
     },
-    
 ];
+
+interface notificationIF {
+    title: string;
+    message: string;
+    icon: notificationType;
+}
 
 // main react fn
 export default function testpage(props: propsIF) {
     false && props;
+
+    function makeNotificationData() {
+        function getRandomElement<T>(a: Array<T>): T {
+            const randomIndex = Math.floor(Math.random() * a.length);
+            return a[randomIndex];
+        }
+        const meta = getRandomElement(notificationMeta);
+        return ({
+            title: meta.title,
+            message: getRandomElement(meta.messages),
+            icon: meta.icon,
+        });
+    }
+
+    const [notifications, setNotifications] = useState<notificationIF[]>([]);
+    function addNotification(): void {
+        const nextNotification: notificationIF = makeNotificationData();
+        setNotifications([
+            ...notifications, nextNotification,
+        ]);
+    }
+
+    useEffect(() => {
+        console.log(notifications);
+    }, [notifications]);
 
     const modalControl: useModalIF = useModal('closed');
 
     return (
         <div className={styles.testpage}>
             {/* interactable to open modal on user action */}
-            <button onClick={() => modalControl.open()}>Open Modal</button>
+            <button onClick={() => addNotification()}>
+                Open Modal
+            </button>
             {/* format to insantiate modal in the DOM */}
-            { modalControl.isOpen &&
-                <Notification
-                    status={'pending'}
-                    dismiss={modalControl.close}
-                />
+            { notifications.length &&
+                <Modal>
+                    {
+                        notifications.map((n: notificationIF, i) => 
+                            <Notification
+                                key={JSON.stringify(n) + i.toString()}
+                                status='pending'
+                                dismiss={() => null}
+                            />
+                        )
+                    }
+                </Modal>
             }
         </div>
     );
