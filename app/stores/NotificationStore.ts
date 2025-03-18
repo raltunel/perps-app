50ca6a68309e54da6def3fa33191ff70872d960f
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-03-18 14:16:00 -0400

    simplify logic to change the max number of notifications in the DOM

--- app/stores/NotificationStore.ts
+++ app/stores/NotificationStore.ts
@@ -156,18 +156,22 @@ export interface NotificationStoreIF {
     clearAll: () => void;
 }
 
+// cap on the number of notifications to manage
+const MAX_NOTIFICATIONS = 3;
+
 // the actual data store
 export const useNotificationStore = create<NotificationStoreIF>((set, get) => ({
     // raw data consumed by the app
     notifications: [],
     // fn to add a new population to state
-    add: (s?: notificationSlugs): void => {
-        const current: notificationIF[] = get().notifications;
-        set({ notifications: [
-                ...current.slice(current.length - 2),
+    add: (s?: notificationSlugs): void => set(
+        {
+            notifications: [
+                ...get().notifications,
                 makeNotificationData(s)
-            ] });
-    },
+            ].slice(-MAX_NOTIFICATIONS),
+        }
+    ),
     // fn to remove an existing element from the data array
     remove: (id: number): void => set({
         notifications: get().notifications.filter((n: notificationIF) => n.oid !== id)
