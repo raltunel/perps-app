bd7b28fb2ec95c1abd182c1b7970ede61dc43b01
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-03-21 14:14:43 -0400

    stub up controls for app settings dropdown

--- app/components/AppOptions/OptionLineSelect.tsx
+++ app/components/AppOptions/OptionLineSelect.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './OptionLineSelect.module.css';
 
 interface propsIF {
@@ -7,9 +8,16 @@ interface propsIF {
 export default function OptionLineSelect(props: propsIF) {
     const { text } = props;
 
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
     return (
         <li className={styles.option_line}>
             {text}
+            {
+                isOpen
+                    ? <div onClick={() => setIsOpen(false)}>Click to Close</div>
+                    : <div onClick={() => setIsOpen(true)}>Click to Open</div>
+            }
         </li>
     );
 }
