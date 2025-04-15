00cf5af578de3033cd3394ba10fb863c381180ec
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-04-15 13:56:50 -0400

    apply prettier

--- app/routes/subaccounts/subaccounts.tsx
+++ app/routes/subaccounts/subaccounts.tsx
@@ -2,8 +2,6 @@ import styles from './subaccounts.module.css';
 
 export default function subaccounts() {
     return (
-        <div className={styles.subaccounts}>
-            This is the sub accounts page
-        </div>
+        <div className={styles.subaccounts}>This is the sub accounts page</div>
     );
 }
