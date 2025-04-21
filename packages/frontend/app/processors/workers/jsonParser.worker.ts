ebe8031741612ca1f1dbd3e73ae050a727d34865
strobie <0xstrobe@protonmail.com>
2025-04-21 23:37:57 +0200

    improve type safety of wokers

--- packages/frontend/app/processors/workers/jsonParser.worker.ts
+++ packages/frontend/app/processors/workers/jsonParser.worker.ts
@@ -1,10 +1,11 @@
-// jsonParser.worker.js
+/// <reference lib="webworker" />
 
-self.onmessage = function (event) {
+self.onmessage = function (event: MessageEvent<string>) {
     try {
         const parsedData = JSON.parse(event.data);
         self.postMessage(parsedData);
     } catch (error) {
+        console.error('Error parsing JSON in jsonParser.worker:', error);
         self.postMessage({ error: (error as Error).message });
     }
 };
--- packages/frontend/app/processors/workers/webdata2.worker.ts
+++ packages/frontend/app/processors/workers/webdata2.worker.ts
@@ -1,4 +1,4 @@
-// jsonParser.worker.js
+/// <reference lib="webworker" />
 
 import type { SymbolInfoIF } from '../../utils/SymbolInfoIFs';
 import { processSymbolInfo } from '../processSymbolInfo';
@@ -8,95 +8,96 @@ import { processPosition } from '../processPosition';
 import type { PositionIF } from '../../utils/position/PositionIFs';
 import { parseNum } from '../../utils/orderbook/OrderBookUtils';
 
-self.onmessage = function (event) {
-    try {
-        const size = event.data.length;
-        const parsedData = JSON.parse(event.data);
-        const coins: SymbolInfoIF[] = [];
-        const userOpenOrders: OrderDataIF[] = [];
-        const positions: PositionIF[] = [];
-        const data = parsedData.data;
-        const tpSlMap: Map<string, { tp: number; sl: number }> = new Map();
-        const coinPriceMap: Map<string, number> = new Map();
+export type Webdata2SendData = string;
+export type Webdata2ReceiveData = {
+    channel: string;
+    data: {
+        coins: SymbolInfoIF[];
+        userOpenOrders: OrderDataIF[];
+        user: any;
+        positions: PositionIF[];
+        coinPriceMap: Map<string, number>;
+    };
+};
 
-        if (data) {
-            if (data.meta && data.meta.universe && data.assetCtxs) {
-                data.meta.universe.forEach((coin: any) => {
-                    const indexOfCoin = data.meta.universe.findIndex(
-                        (item: any) => item.name === coin.name,
-                    );
-                    const ctxVal = data.assetCtxs[indexOfCoin];
-                    if (ctxVal !== null) {
-                        const coinObject = processSymbolInfo({
-                            coin: coin.name,
-                            szDecimals: coin.szDecimals,
-                            maxLeverage: coin.maxLeverage,
-                            ctx: ctxVal,
-                        });
-                        coins.push(coinObject);
-                        coinPriceMap.set(coin.name, coinObject.markPx);
-                    }
-                });
-                data.openOrders.forEach((order: any) => {
-                    const processedOrder = processUserOrder(order, 'open');
-                    if (processedOrder) {
-                        userOpenOrders.push(processedOrder);
-                        if (processedOrder.isPositionTpsl) {
-                            if (
-                                processedOrder.orderType?.indexOf(
-                                    'Take Profit',
-                                ) !== -1
-                            ) {
-                                tpSlMap.set(processedOrder.coin, {
-                                    tp: parseNum(processedOrder.triggerPx || 0),
-                                    sl:
-                                        tpSlMap.get(processedOrder.coin)?.sl ||
-                                        0,
-                                });
-                            } else if (
-                                processedOrder.orderType?.indexOf('Stop') !== -1
-                            ) {
-                                tpSlMap.set(processedOrder.coin, {
-                                    tp:
-                                        tpSlMap.get(processedOrder.coin)?.tp ||
-                                        0,
-                                    sl: parseNum(processedOrder.triggerPx || 0),
-                                });
-                            }
+self.onmessage = function (event: MessageEvent<string>) {
+    const parsedData = JSON.parse(event.data);
+    const coins: SymbolInfoIF[] = [];
+    const userOpenOrders: OrderDataIF[] = [];
+    const positions: PositionIF[] = [];
+    const data = parsedData.data;
+    const tpSlMap: Map<string, { tp: number; sl: number }> = new Map();
+    const coinPriceMap: Map<string, number> = new Map();
+
+    if (data) {
+        if (data.meta && data.meta.universe && data.assetCtxs) {
+            data.meta.universe.forEach((coin: any) => {
+                const indexOfCoin = data.meta.universe.findIndex(
+                    (item: any) => item.name === coin.name,
+                );
+                const ctxVal = data.assetCtxs[indexOfCoin];
+                if (ctxVal !== null) {
+                    const coinObject = processSymbolInfo({
+                        coin: coin.name,
+                        szDecimals: coin.szDecimals,
+                        maxLeverage: coin.maxLeverage,
+                        ctx: ctxVal,
+                    });
+                    coins.push(coinObject);
+                    coinPriceMap.set(coin.name, coinObject.markPx);
+                }
+            });
+            data.openOrders.forEach((order: any) => {
+                const processedOrder = processUserOrder(order, 'open');
+                if (processedOrder) {
+                    userOpenOrders.push(processedOrder);
+                    if (processedOrder.isPositionTpsl) {
+                        if (
+                            processedOrder.orderType?.indexOf('Take Profit') !==
+                            -1
+                        ) {
+                            tpSlMap.set(processedOrder.coin, {
+                                tp: parseNum(processedOrder.triggerPx || 0),
+                                sl: tpSlMap.get(processedOrder.coin)?.sl || 0,
+                            });
+                        } else if (
+                            processedOrder.orderType?.indexOf('Stop') !== -1
+                        ) {
+                            tpSlMap.set(processedOrder.coin, {
+                                tp: tpSlMap.get(processedOrder.coin)?.tp || 0,
+                                sl: parseNum(processedOrder.triggerPx || 0),
+                            });
                         }
                     }
-                });
-
-                if (data.clearinghouseState) {
-                    data.clearinghouseState.assetPositions.forEach(
-                        (position: any) => {
-                            const processedPosition = processPosition(position);
-                            if (tpSlMap.has(processedPosition.coin)) {
-                                const existingOrder = tpSlMap.get(
-                                    processedPosition.coin,
-                                );
-                                processedPosition.tp = existingOrder?.tp || 0;
-                                processedPosition.sl = existingOrder?.sl || 0;
-                            }
-                            positions.push(processedPosition);
-                        },
-                    );
                 }
+            });
+
+            if (data.clearinghouseState) {
+                data.clearinghouseState.assetPositions.forEach(
+                    (position: any) => {
+                        const processedPosition = processPosition(position);
+                        if (tpSlMap.has(processedPosition.coin)) {
+                            const existingOrder = tpSlMap.get(
+                                processedPosition.coin,
+                            );
+                            processedPosition.tp = existingOrder?.tp || 0;
+                            processedPosition.sl = existingOrder?.sl || 0;
+                        }
+                        positions.push(processedPosition);
+                    },
+                );
             }
         }
-
-        self.postMessage({
-            channel: parsedData.channel,
-            data: {
-                coins,
-                userOpenOrders,
-                user: data.user,
-                size,
-                positions,
-                coinPriceMap,
-            },
-        });
-    } catch (error) {
-        self.postMessage({ error: (error as Error).message });
     }
+
+    self.postMessage({
+        channel: parsedData.channel,
+        data: {
+            coins,
+            userOpenOrders,
+            user: data.user,
+            positions,
+            coinPriceMap,
+        },
+    });
 };
