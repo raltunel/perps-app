81ecfc3971e57ece4878951eb41eafa9d3a67ee2
Ridvan Altunel <raltunel@proven.com.tr>
2025-04-22 17:35:37 +0300

    change init of worker

--- packages/frontend/app/hooks/useWorkerAgenda.ts
+++ packages/frontend/app/hooks/useWorkerAgenda.ts
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useCallback, type RefObject } from 'react';
 
+import webData2Worker from './../processors/workers/webdata2.worker.ts?worker';
+
 const getWorkerPath = (type: string) => {
     switch (type) {
         case 'webData2':
@@ -9,18 +11,29 @@ const getWorkerPath = (type: string) => {
     }
 };
 
+const getWorker = (type: string): Worker | null => {
+    switch (type) {
+        case 'webData2':
+            return new webData2Worker();
+        default:
+            return null;
+    }
+};
 export const useWorkerAgenda = (): {
     checkCustomWorker: (type: string) => Worker | null;
 } => {
     const checkCustomWorker = (type: string) => {
-        const workerPath = getWorkerPath(type);
-        if (workerPath.length > 0) {
-            const worker = new Worker(new URL(workerPath, import.meta.url), {
-                type: 'module',
-            });
-            return worker;
-        }
-        return null;
+        // const workerPath = getWorkerPath(type);
+        // if (workerPath.length > 0) {
+        //     const worker = new Worker(new URL(workerPath, import.meta.url), {
+        //         type: 'module',
+        //     });
+        //     return worker;
+        // }
+        // return null;
+
+        const worker = getWorker(type);
+        return worker;
     };
 
     return {
