102167f81cbe8cf412f811208b6fb7ade2bc8fec
Ben Wolski <570819+benwolski@users.noreply.github.com>
2025-05-06 15:39:11 -0400

    fix linting erros in vite config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,66 +1,71 @@
 import { reactRouter } from '@react-router/dev/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption, type UserConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 const appName = 'Ambient Perps';
 const appDescription = 'A modern, performant app for perpetual contracts.';
 
-export default defineConfig(({ isSsrBuild }) => ({
-    build: {
-        rollupOptions: isSsrBuild
-            ? {
-                  input: './server/app.ts',
-              }
-            : undefined,
-    },
+const ssrEntry = './server/app.ts';
 
-    plugins: [
-        reactRouter(),
-        tsconfigPaths(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            workbox: {
-                maximumFileSizeToCacheInBytes: 3000000,
-            },
-            devOptions: {
-                enabled: !isSsrBuild && process.env.NODE_ENV === 'development',
-            },
-            manifest: {
-                name: appName,
-                short_name: appName,
-                description: appDescription,
-                theme_color: '#7371fc',
-                background_color: '#7371fc',
-                display: 'standalone',
-                start_url: '/',
-                id: '/',
-                lang: 'en',
-                orientation: 'portrait',
-                icons: [
-                    {
-                        src: '/images/pwa-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png',
-                    },
-                    {
-                        src: '/images/pwa-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                    },
-                    {
-                        src: '/images/pwa-64x64.png',
-                        sizes: '64x64',
-                        type: 'image/png',
-                    },
-                    {
-                        src: '/images/maskable-icon-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'maskable',
-                    },
-                ],
-            },
-        }),
-    ],
-}));
+export default defineConfig(
+    ({ isSsrBuild }): UserConfig => ({
+        build: {
+            rollupOptions: isSsrBuild
+                ? {
+                      input: ssrEntry,
+                  }
+                : undefined,
+        },
+
+        plugins: [
+            reactRouter(),
+            tsconfigPaths() as PluginOption,
+            VitePWA({
+                registerType: 'autoUpdate',
+                workbox: {
+                    maximumFileSizeToCacheInBytes: 3000000,
+                },
+                devOptions: {
+                    enabled:
+                        !isSsrBuild && process.env.NODE_ENV === 'development',
+                },
+                manifest: {
+                    name: appName,
+                    short_name: appName,
+                    description: appDescription,
+                    theme_color: '#7371fc',
+                    background_color: '#7371fc',
+                    display: 'standalone',
+                    start_url: '/',
+                    id: '/',
+                    lang: 'en',
+                    orientation: 'portrait',
+                    icons: [
+                        {
+                            src: '/images/pwa-192x192.png',
+                            sizes: '192x192',
+                            type: 'image/png',
+                        },
+                        {
+                            src: '/images/pwa-512x512.png',
+                            sizes: '512x512',
+                            type: 'image/png',
+                        },
+                        {
+                            src: '/images/pwa-64x64.png',
+                            sizes: '64x64',
+                            type: 'image/png',
+                        },
+                        {
+                            src: '/images/maskable-icon-512x512.png',
+                            sizes: '512x512',
+                            type: 'image/png',
+                            purpose: 'maskable',
+                        },
+                    ],
+                },
+            }),
+        ],
+    }),
+);
