dc940a9ac24e1897bbceea02f1db311a1d5e9cbf
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-05-22 09:58:49 -0400

    fix strategy detail page styling

--- packages/frontend/app/routes/strategies/strategies.tsx
+++ packages/frontend/app/routes/strategies/strategies.tsx
@@ -8,15 +8,13 @@ export default function Strategies() {
 
     return (
         <div className={styles.strategies_page}>
-            <h2>Strategies</h2>
+            <h2>Strategy Name</h2>
             <p className={styles.strategies_blurb}>
                 Run an automated market making strategy
             </p>
             <div className={styles.strategies_learn_more}>Learn more</div>
             <div className={styles.strategy_select}>
                 <div className={styles.strategy_select_left}>
-                    <label>Select Strategy</label>
-                    <div>Tight!</div>
                     <Button
                         onClick={() => console.log('Strategy Paused!')}
                         size='medium'
