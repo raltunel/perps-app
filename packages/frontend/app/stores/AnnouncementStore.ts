69e878ac52f2377b3375ac6a10f41df07222f399
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-06-24 18:34:35 -0400

    annotate `AnnouncementStore.ts` file

--- packages/frontend/app/stores/AnnouncementStore.ts
+++ packages/frontend/app/stores/AnnouncementStore.ts
@@ -11,7 +11,9 @@ export interface AnnouncementStoreIF {
 export const useAnnouncementStore = create<AnnouncementStoreIF>((set, get) => ({
     // raw data consumed by the app
     wasViewed: [],
+    // method to record an announcement as viewed
     markViewed: (a: string): void =>
         set({ wasViewed: [...get().wasViewed, a] }),
+    // method to determine whether an announcement was previously viewed
     checkIfViewed: (a: string): boolean => get().wasViewed.includes(a),
 }));
