c6a761ab1955cf7625475114a59ffe6541d1ff23
Ridvan Altunel <raltunel@proven.com.tr>
2025-07-03 10:39:54 +0300

    add logger for ping

--- packages/sdk/src/ws.ts
+++ packages/sdk/src/ws.ts
@@ -201,6 +201,7 @@ export class WebsocketManager {
     };
 
     private sendPing = () => {
+        console.log('>>> sendPing', new Date().toISOString());
         if (this.stopped || !this.ws || this.ws.readyState !== WebSocket.OPEN)
             return;
         this.log('sending ping');
