5ecf63aa3ec1e253f7264201c8131d3a1e253a19
Ben Wolski <570819+benwolski@users.noreply.github.com>
2025-07-18 19:15:04 -0400

    comment out domain override

--- packages/frontend/app/root.tsx
+++ packages/frontend/app/root.tsx
@@ -119,7 +119,7 @@ export default function App() {
                     endpoint='https://testnet.fogo.io/'
                     sponsor='8HnaXmgFJbvvJxSdjeNyWwMXZb85E35NM4XNg6rxuw3w'
                     paymasterUrl='https://sessions-example.fogo.io/paymaster'
-                    domain='https://perps.ambient.finance'
+                    // domain='https://perps.ambient.finance' // enable for local development
                     tokens={[NATIVE_MINT.toBase58()]}
                     defaultRequestedLimits={{
                         [NATIVE_MINT.toBase58()]: 1_500_000_000n,
