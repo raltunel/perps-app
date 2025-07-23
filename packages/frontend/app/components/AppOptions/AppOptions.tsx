9d4e704bb57ecfb6b7ad0dbaeb8eed58a3469210
Jadessu <junioradessu@gmail.com>
2025-07-23 15:20:43 -0500

    refactor app options to only keep needed options

--- packages/frontend/app/components/AppOptions/AppOptions.tsx
+++ packages/frontend/app/components/AppOptions/AppOptions.tsx
@@ -61,6 +61,7 @@ export default function AppOptions() {
                     }
                 /> */}
             </ul>
+            {/* divider */}
             {/* <div className={styles.horizontal_divider} /> */}
             <ul>
                 {/* <OptionLine
