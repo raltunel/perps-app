190088a27d1d348661fee10d53e4ba598ad87e9d
Ben Wolski <570819+benwolski@users.noreply.github.com>
2025-07-24 11:10:37 -0400

    revert upper casing

--- packages/frontend/app/routes/trade.tsx
+++ packages/frontend/app/routes/trade.tsx
@@ -122,7 +122,7 @@ export default function Trade() {
 
     useEffect(() => {
         if (!marketId)
-            navigate(`/v2/trade/${symbol.toUpperCase()}`, {
+            navigate(`/v2/trade/${symbol}`, {
                 replace: true,
                 viewTransition: true,
             });
