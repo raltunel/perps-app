97b0bf4406b50e4350eb9fc7be7f1ed82f6cdd0e
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-07-28 10:29:43 -0400

    refactor CSS and JSX structure for `MarginModal.tsx` file

--- packages/frontend/app/components/Trade/OrderInput/MarginModal/MarginModal.tsx
+++ packages/frontend/app/components/Trade/OrderInput/MarginModal/MarginModal.tsx
@@ -11,31 +11,33 @@ export default function MarginModal(props: propsIF) {
 
     return (
         <section className={styles.margin_modal_content}>
-            <button
-                className={styles[initial === 'cross' ? 'selected' : '']}
-                onClick={() => handleConfirm('cross')}
-            >
-                <h3>Cross Margin</h3>
-                <p>
-                    All cross positions share the same cross margin as
-                    collateral. In the event of liquidation, your cross margin
-                    balance and any remaining open positions under assets in
-                    this mode may be forfeited.
-                </p>
-            </button>
-            <button
-                className={styles[initial === 'isolated' ? 'selected' : '']}
-                onClick={() => handleConfirm('isolated')}
-            >
-                <h3>Isolated Mode</h3>
-                <p>
-                    Manage your risk on individual positions by restricting the
-                    amount of margin allocated to each. If the margin ratio of
-                    an isolated position reaches 100%, the position will be
-                    liquidated. Margin can be added or removed to individual
-                    positions in this mode.
-                </p>
-            </button>
+            <div className={styles.margin_buttons}>
+                <button
+                    className={styles[initial === 'cross' ? 'selected' : '']}
+                    onClick={() => handleConfirm('cross')}
+                >
+                    <h3>Cross Margin</h3>
+                    <p>
+                        All cross positions share the same cross margin as
+                        collateral. In the event of liquidation, your cross
+                        margin balance and any remaining open positions under
+                        assets in this mode may be forfeited.
+                    </p>
+                </button>
+                <button
+                    className={styles[initial === 'isolated' ? 'selected' : '']}
+                    onClick={() => handleConfirm('isolated')}
+                >
+                    <h3>Isolated Mode</h3>
+                    <p>
+                        Manage your risk on individual positions by restricting
+                        the amount of margin allocated to each. If the margin
+                        ratio of an isolated position reaches 100%, the position
+                        will be liquidated. Margin can be added or removed to
+                        individual positions in this mode.
+                    </p>
+                </button>
+            </div>
         </section>
     );
 }
