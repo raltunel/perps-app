5d071e599bef3aa5721836b9339cec983a98a559
Ben Wolski <570819+benwolski@users.noreply.github.com>
2025-08-01 15:02:13 -0400

    add mode for automatically updating limit price to mid price

--- packages/frontend/app/components/Trade/OrderInput/OrderInput.tsx
+++ packages/frontend/app/components/Trade/OrderInput/OrderInput.tsx
@@ -222,7 +222,7 @@ function OrderInput({
         return midPrice;
     };
 
-    useEffect(() => {
+    const setMidPriceAsPriceInput = () => {
         if (
             marketOrderType === 'limit' &&
             buys.length > 0 &&
@@ -237,7 +237,28 @@ function OrderInput({
             );
             setPrice(formattedMidPrice);
         }
+    };
+
+    useEffect(() => {
+        // set mid price input as default price when market changes
+        setMidPriceAsPriceInput();
+        setIsMidModeActive(false);
     }, [marketOrderType, !buys.length, !sells.length, buys?.[0]?.coin]);
+
+    const [isMidModeActive, setIsMidModeActive] = useState(false);
+
+    useEffect(() => {
+        if (isMidModeActive) {
+            setMidPriceAsPriceInput();
+        }
+    }, [
+        isMidModeActive,
+        marketOrderType,
+        !buys.length,
+        !sells.length,
+        buys?.[0]?.px,
+        sells?.[0]?.px,
+    ]);
 
     const confirmOrderModal = useModal<modalContentT>('closed');
 
@@ -894,9 +915,19 @@ function OrderInput({
             className: 'custom-input',
             ariaLabel: 'Price input',
             showMidButton: ['stop_limit', 'limit'].includes(marketOrderType),
-            getMidPrice,
+            setMidPriceAsPriceInput,
+            isMidModeActive,
+            setIsMidModeActive,
         }),
-        [price, handlePriceChange, marketOrderType, markPx],
+        [
+            price,
+            handlePriceChange,
+            marketOrderType,
+            markPx,
+            isMidModeActive,
+            setIsMidModeActive,
+            setMidPriceAsPriceInput,
+        ],
     );
 
     const sizeInputProps = useMemo(
