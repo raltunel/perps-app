669e0c7e9ed7f5f7c1f3f7b575db1d0b43f903ae
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-08-04 17:29:10 -0400

    carry over changes to withdraw modal

--- packages/frontend/app/routes/testpage/testpage.tsx
+++ packages/frontend/app/routes/testpage/testpage.tsx
@@ -1,16 +1,16 @@
-import PortfolioDeposit from '~/components/Portfolio/PortfolioDeposit/PortfolioDeposit';
+import PortfolioWithdraw from '~/components/Portfolio/PortfolioWithdraw/PortfolioWithdraw';
 import styles from './testpage.module.css';
 
 export default function testpage() {
     return (
         <div className={styles.testpage}>
-            <PortfolioDeposit
+            <PortfolioWithdraw
                 portfolio={{
                     id: 'portfolio_deposit_testpage',
                     name: 'sample_portfolio',
                     availableBalance: 83,
                 }}
-                onDeposit={() => {}}
+                onWithdraw={() => {}}
                 onClose={() => {}}
             />
         </div>
