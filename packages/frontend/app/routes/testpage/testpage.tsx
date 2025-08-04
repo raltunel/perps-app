cf24c8d2429f54061660ecfab60b5c0d98cb9653
Jadessu <junioradessu@gmail.com>
2025-08-04 16:04:17 -0500

    to resume

--- packages/frontend/app/routes/testpage/testpage.tsx
+++ packages/frontend/app/routes/testpage/testpage.tsx
@@ -1,9 +1,15 @@
+import { useModal } from '~/hooks/useModal';
 import styles from './testpage.module.css';
+import LimitChaseModal from '~/components/Trade/LimitChaseModal/LimitChaseModal';
 
 export default function testpage() {
+    const modalCtrl = useModal('closed');
+
     return (
         <div className={styles.testpage}>
-            <input type='text' />
+            <button onClick={modalCtrl.open}>Open Limit Chase Modal</button>
+
+            {modalCtrl.isOpen && <LimitChaseModal close={modalCtrl.close} />}
         </div>
     );
 }
