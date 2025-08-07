2bbeac43a228edc2e474d640ef3a21772850b63e
tcoskun <tcoskun@proven.com.tr>
2025-08-07 14:23:31 +0300

    fix: revert order line if reposition fails on drag

--- packages/frontend/app/routes/chart/orders/component/LabelComponent.ts
+++ packages/frontend/app/routes/chart/orders/component/LabelComponent.ts
@@ -511,6 +511,7 @@ const LabelComponent = ({
                     await executeLimitOrder(newOrderParams);
 
                 if (!limitOrderResult.success) {
+                    setSelectedLine(undefined);
                     console.error(
                         'Failed to create new order:',
                         limitOrderResult.error,
@@ -540,6 +541,7 @@ const LabelComponent = ({
                     });
                 }
             } catch (error) {
+                setSelectedLine(undefined);
                 console.error('Error updating order:', error);
                 add({
                     title: 'Error updating order',
