762fac1847d7d6944514757cdc1991c78ad9d943
Ben Wolski <570819+benwolski@users.noreply.github.com>
2025-08-13 22:22:58 -0400

    update version check frequency

--- packages/frontend/app/hooks/useVersionCheck.ts
+++ packages/frontend/app/hooks/useVersionCheck.ts
@@ -4,6 +4,10 @@ export function useVersionCheck() {
     const [showReload, setShowReload] = useState(false);
     const currentVersion = useRef(null);
 
+    const isProduction = process.env.CONTEXT === 'production';
+    const isDeployPreview = process.env.CONTEXT === 'deploy-preview';
+    const isBranchDeploy = process.env.CONTEXT === 'branch-deploy';
+
     useEffect(() => {
         fetch('/version.json', { cache: 'no-store' })
             .then((res) => res.json())
@@ -25,7 +29,11 @@ export function useVersionCheck() {
                         }
                     });
             },
-            5 * 60 * 1000, // Check every 5 minutes
+            isProduction
+                ? 5 * 60 * 1000
+                : isDeployPreview || isBranchDeploy
+                  ? 30 * 1000
+                  : 0, // Check every 5 minutes in production, every 30 seconds in deploy preview or branch-deploy and never if not on any of those
         );
 
         return () => clearInterval(interval);
