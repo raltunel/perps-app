ab8b05f5d451871c02f852f3da0f2a561df71399
Ben Wolski <570819+benwolski@users.noreply.github.com>
2025-08-19 01:01:32 -0400

    add more granular duration segments

--- packages/frontend/app/utils/functions/getDurationSegment.ts
+++ packages/frontend/app/utils/functions/getDurationSegment.ts
@@ -13,47 +13,58 @@ export const getDurationSegment = (
                 ? '< 1s'
                 : durationSeconds < 2
                   ? '< 2s'
-                  : durationSeconds < 5
-                    ? '< 5s'
-                    : durationSeconds < 10
-                      ? '< 10s'
-                      : durationSeconds < 15
-                        ? '< 15s'
-                        : durationSeconds < 20
-                          ? '< 20s'
-                          : durationSeconds < 25
-                            ? '< 25s'
-                            : durationSeconds < 30
-                              ? '< 30s'
-                              : durationSeconds < 35
-                                ? '< 35s'
-                                : durationSeconds < 40
-                                  ? '< 40s'
-                                  : durationSeconds < 45
-                                    ? '< 45s'
-                                    : durationSeconds < 50
-                                      ? '< 50s'
-                                      : durationSeconds < 55
-                                        ? '< 55s'
-                                        : durationSeconds < 60
-                                          ? '< 1m'
-                                          : durationSeconds < 120
-                                            ? '< 2m'
-                                            : durationSeconds < 300
-                                              ? '< 5m'
-                                              : durationSeconds < 600
-                                                ? '< 10m'
-                                                : durationSeconds < 900
-                                                  ? '< 15m'
-                                                  : durationSeconds < 1800
-                                                    ? '< 30m'
-                                                    : durationSeconds < 3600
-                                                      ? '< 1h'
-                                                      : durationSeconds < 7200
-                                                        ? '< 2h'
-                                                        : durationSeconds <
-                                                            86400
-                                                          ? '< 1d'
-                                                          : 'unknown';
+                  : durationSeconds < 3
+                    ? '< 3s'
+                    : durationSeconds < 4
+                      ? '< 4s'
+                      : durationSeconds < 5
+                        ? '< 5s'
+                        : durationSeconds < 7
+                          ? '< 7s'
+                          : durationSeconds < 10
+                            ? '< 10s'
+                            : durationSeconds < 15
+                              ? '< 15s'
+                              : durationSeconds < 20
+                                ? '< 20s'
+                                : durationSeconds < 25
+                                  ? '< 25s'
+                                  : durationSeconds < 30
+                                    ? '< 30s'
+                                    : durationSeconds < 35
+                                      ? '< 35s'
+                                      : durationSeconds < 40
+                                        ? '< 40s'
+                                        : durationSeconds < 45
+                                          ? '< 45s'
+                                          : durationSeconds < 50
+                                            ? '< 50s'
+                                            : durationSeconds < 55
+                                              ? '< 55s'
+                                              : durationSeconds < 60
+                                                ? '< 1m'
+                                                : durationSeconds < 90
+                                                  ? '< 1.5m'
+                                                  : durationSeconds < 120
+                                                    ? '< 2m'
+                                                    : durationSeconds < 300
+                                                      ? '< 5m'
+                                                      : durationSeconds < 600
+                                                        ? '< 10m'
+                                                        : durationSeconds < 900
+                                                          ? '< 15m'
+                                                          : durationSeconds <
+                                                              1800
+                                                            ? '< 30m'
+                                                            : durationSeconds <
+                                                                3600
+                                                              ? '< 1h'
+                                                              : durationSeconds <
+                                                                  7200
+                                                                ? '< 2h'
+                                                                : durationSeconds <
+                                                                    86400
+                                                                  ? '< 1d'
+                                                                  : 'unknown';
     return durationSegment;
 };
