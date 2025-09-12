80ec0185eb73be3b2c3baed272b3d4e0f313619d
Ben Wolski <570819+benwolski@users.noreply.github.com>
2025-09-12 17:07:35 -0400

    add MID as universal translation for mid price

--- packages/frontend/app/components/Trade/OrderInput/PriceInput/PriceInput.tsx
+++ packages/frontend/app/components/Trade/OrderInput/PriceInput/PriceInput.tsx
@@ -85,7 +85,7 @@ export default function PriceInput(props: PropsIF) {
                         }
                     }}
                 >
-                    Mid
+                    {t('transactions.midButton')}
                 </button>
             )}
         </div>
