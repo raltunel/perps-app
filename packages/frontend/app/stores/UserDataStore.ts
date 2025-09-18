9e608928c732b7c7a568f602c0d30ec584f6aade
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-09-18 16:58:25 -0400

    annotate new code in `UserDataStore.ts` file

--- packages/frontend/app/stores/UserDataStore.ts
+++ packages/frontend/app/stores/UserDataStore.ts
@@ -21,6 +21,7 @@ const LS_KEY = 'USER_DATA';
 export const useUserDataStore = create<UserDataStore>()(
     persist(
         (set, get) => {
+            // private helper fn to set the ref code and validation status
             function _setRefCode(r: string, v: boolean) {
                 set({
                     refCode: {
@@ -29,6 +30,7 @@ export const useUserDataStore = create<UserDataStore>()(
                     },
                 });
             }
+            // obj returned to app when store is instantiated
             return {
                 userAddress: '',
                 setUserAddress: (userAddress: string) => {
@@ -37,28 +39,36 @@ export const useUserDataStore = create<UserDataStore>()(
                 referralCode: '',
                 setReferralCode: (r: string) => set({ referralCode: r }),
                 clearReferralCode: () => set({ referralCode: '' }),
+                // string value of the ref code and whether it has been validated
                 refCode: {
                     value: '',
                     isValidated: false,
                 },
+                // record a new referral code to store
                 setRefCode(r: string): void {
                     _setRefCode(r, false);
                 },
+                // record a new referral code to store if different from the last
                 initializeRefCode(r: string): void {
                     const lastCode: string | null = get().refCode.value;
                     if (lastCode !== r) {
                         _setRefCode(r, false);
                     }
                 },
+                // mark the referral code as validated
                 validateRefCode(): void {
                     _setRefCode(get().refCode.value, true);
                 },
             };
         },
         {
+            // local storage key for persisted data
             name: LS_KEY,
+            // storage engine
             storage: createJSONStorage(() => localStorage),
+            // version number (needed for migrations)
             version: 1,
+            // data object to persist in local storage
             partialize: (state: UserDataStore) => ({
                 referralCode: state.referralCode,
                 refCode: state.refCode,
