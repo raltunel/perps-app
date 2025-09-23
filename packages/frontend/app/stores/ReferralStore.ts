399c33f1a745fc305bb7e9a73d2bc4866260b938
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-09-23 18:25:38 -0400

    create new zustand store for referral data

---
+++ packages/frontend/app/stores/ReferralStore.ts
@@ -0,0 +1,60 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+interface RefCode {
+    value: string;
+    isConfirmed: boolean;
+    wallet: string;
+}
+
+export interface ReferralStore {
+    active: RefCode | null;
+    all: RefCode[];
+    add(rc: RefCode): void;
+}
+
+const LS_KEY = 'AFFILIATE_DATA';
+
+const ssrSafeStorage = () =>
+    (typeof window !== 'undefined'
+        ? window.localStorage
+        : {
+              getItem: (_key: string) => null,
+              setItem: (_key: string, _value: string) => {},
+              removeItem: (_key: string) => {},
+              clear: () => {},
+              key: (_index: number) => null,
+              length: 0,
+          }) as Storage;
+
+export const useUserDataStore = create<ReferralStore>()(
+    persist(
+        (set, get) => {
+            // obj returned to app when store is instantiated
+            return {
+                active: null,
+                all: [],
+                add(rc: RefCode): void {
+                    const allRefCodes: RefCode[] = get().all;
+                    set({
+                        active: rc,
+                        all: [
+                            rc,
+                            ...allRefCodes.filter(
+                                (r: RefCode) =>
+                                    r.value.toLowerCase() !==
+                                    rc.value.toLowerCase(),
+                            ),
+                        ],
+                    });
+                },
+            };
+        },
+        {
+            // local storage key for persisted data
+            name: LS_KEY,
+            storage: createJSONStorage(ssrSafeStorage),
+            version: 1,
+        },
+    ),
+);
