ea9d51968cf9f0fd567e31d492e40758743bbad2
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-09-24 13:03:40 -0400

    stub up content in referral code modal

--- packages/frontend/app/components/PageHeader/ReferralCodeModal/ReferralCodeModal.tsx
+++ packages/frontend/app/components/PageHeader/ReferralCodeModal/ReferralCodeModal.tsx
@@ -1,9 +1,17 @@
+import SimpleButton from '~/components/SimpleButton/SimpleButton';
 import styles from './ReferralCodeModal.module.css';
 
 export default function ReferralCodeModal() {
     return (
         <section className={styles.referral_code_modal}>
-            Referral Code Modal
+            <p>
+                Your referral code is REF_CODE_HERE for this wallet address. Do
+                you wish to confirm?
+            </p>
+            <div className={styles.button_container}>
+                <SimpleButton>Confirm</SimpleButton>
+                <SimpleButton>Details</SimpleButton>
+            </div>
         </section>
     );
 }
