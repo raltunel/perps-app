0360e9d0458cf1f76bcb70b60e5caee672b73956
zcelbir <zcelbir@proven.com.tr>
2025-11-27 16:27:46 +0300

    solve merge conflict

--- packages/frontend/app/components/Trade/TradeTables/TradeTables.tsx
+++ packages/frontend/app/components/Trade/TradeTables/TradeTables.tsx
@@ -25,6 +25,18 @@ export interface FilterOption {
     label: string;
 }
 
+const tradePageBlackListTabs = new Set([
+    'Funding History',
+    'Deposits and Withdrawals',
+    'Depositors',
+]);
+
+const portfolioPageBlackListTabs = new Set([
+    'Depositors',
+    'Funding History',
+    'Deposits and Withdrawals',
+]);
+
 interface TradeTableProps {
     vaultPage?: boolean;
     vaultFetched?: boolean;
@@ -84,8 +96,19 @@ export default function TradeTable(props: TradeTableProps) {
             availableTabs.push('Depositors');
         }
 
-        // No blacklist needed anymore
-        const filteredTabs = availableTabs;
+        // Filter for different pages
+        let filteredTabs;
+        if (page === Pages.TRADE) {
+            filteredTabs = availableTabs.filter(
+                (tab) => !tradePageBlackListTabs.has(tab),
+            );
+        } else if (page === Pages.PORTFOLIO) {
+            filteredTabs = availableTabs.filter(
+                (tab) => !portfolioPageBlackListTabs.has(tab),
+            );
+        } else {
+            filteredTabs = availableTabs;
+        }
 
         if (isMobile && !props.mobileExternalSwitcher) {
             return filteredTabs.filter(
