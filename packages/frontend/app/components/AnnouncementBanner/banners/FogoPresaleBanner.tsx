a208d726f2ebcf39db7fc0b0fe6d05d678fd93f8
Emily Drake Smith <emily.drake.smith@gmail.com>
2025-12-09 15:51:17 -0500

    toggle FOGO presale banners in active DOM based on viewport width

--- packages/frontend/app/components/AnnouncementBanner/banners/FogoPresaleBanner.tsx
+++ packages/frontend/app/components/AnnouncementBanner/banners/FogoPresaleBanner.tsx
@@ -1,19 +1,65 @@
 import styles from './FogoPresaleBanner.module.css';
-import { PresaleBannerWide } from '~/assets';
+import {
+    PresaleBannerMedium,
+    PresaleBannerNarrow,
+    PresaleBannerWide,
+} from '~/assets';
+import useMediaQuery from '~/hooks/useMediaQuery';
 import { useBannerSVG } from '~/routes/testpage/testpage';
 
 export default function FogoPresaleBanner() {
-    const banner = useBannerSVG('fogo-presale-banner-wide-clickable', () =>
-        console.log('clicked page header banner'),
+    // hooks to attach click handlers to SVGs
+    const bannerWide = useBannerSVG('fogo-presale-banner-wide-clickable', () =>
+        console.log('clicked WIDE page header banner'),
+    );
+    const bannerMedium = useBannerSVG(
+        'fogo-presale-banner-medium-clickable',
+        () => console.log('clicked MEDIUM page header banner'),
+    );
+    const bannerNarrow = useBannerSVG(
+        'fogo-presale-banner-narrow-clickable',
+        () => console.log('clicked NARROW page header banner'),
+    );
+
+    // screen width breakpoints (px) to change the SVG in the active DOM
+    const MIN_VP_WIDTH_FOR_LARGE_BANNER = 1440;
+    const MIN_VP_WIDTH_FOR_MEDIUM_BANNER = 440;
+
+    // media query hooks to determine which elem should be displayed
+    const is1440pxOrLarger = useMediaQuery(
+        `(min-width: ${MIN_VP_WIDTH_FOR_LARGE_BANNER}px)`,
+    );
+    const is440pxOrLarger = useMediaQuery(
+        `(min-width: ${MIN_VP_WIDTH_FOR_MEDIUM_BANNER}px)`,
     );
 
     return (
         <div className={styles.fogo_header_banner_wrapper}>
             <object
-                ref={banner.ref}
+                ref={bannerWide.ref}
                 type='image/svg+xml'
                 data={PresaleBannerWide}
-                // style={{ display: 'block' }}
+                style={{ display: is1440pxOrLarger ? 'block' : 'none' }}
+            />
+            <object
+                ref={bannerMedium.ref}
+                type='image/svg+xml'
+                data={PresaleBannerMedium}
+                style={{
+                    display:
+                        !is1440pxOrLarger && is440pxOrLarger ? 'block' : 'none',
+                }}
+            />
+            <object
+                ref={bannerNarrow.ref}
+                type='image/svg+xml'
+                data={PresaleBannerNarrow}
+                style={{
+                    display:
+                        !is1440pxOrLarger && !is440pxOrLarger
+                            ? 'block'
+                            : 'none',
+                }}
             />
         </div>
     );
