e51040dee7b0b8b312c3ab2f3257e476f9cb86ce
Jadessu <junioradessu@gmail.com>
2025-12-10 11:43:38 -0600

    fix banner mobile layout

--- packages/frontend/app/components/AnnouncementBanner/banners/FogoPresaleBanner.tsx
+++ packages/frontend/app/components/AnnouncementBanner/banners/FogoPresaleBanner.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import styles from './FogoPresaleBanner.module.css';
 import { PresaleBannerWide } from '~/assets';
 
@@ -5,10 +6,16 @@ export default function FogoPresaleBanner() {
     const PRESALE_URL = 'https://presale.fogo.io/';
 
     return (
-        <div className={styles.fogo_header_banner_wrapper}>
-            <a href={PRESALE_URL} target='_blank' rel='noopener noreferrer'>
-                <img src={PresaleBannerWide} alt='Fogo Presale Banner' />
-            </a>
-        </div>
+        <motion.a
+            href={PRESALE_URL}
+            target='_blank'
+            rel='noopener noreferrer'
+            className={styles.fogo_header_banner_wrapper}
+            initial={{ clipPath: 'inset(0 100% 0 0)' }}
+            animate={{ clipPath: 'inset(0 0% 0 0)' }}
+            transition={{ duration: 1.2, ease: 'easeOut' }}
+        >
+            <img src={PresaleBannerWide} alt='Fogo Presale Banner' />
+        </motion.a>
     );
 }
