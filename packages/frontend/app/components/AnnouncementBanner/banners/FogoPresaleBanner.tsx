13e9cfcc62eeedc9c2fe6f4a67273ebf92fdb6e9
Ben Wolski <570819+benwolski@users.noreply.github.com>
2025-12-09 20:45:06 -0500

    wrap anchor tag around img element and adjust CSS for better banner scaling

--- packages/frontend/app/components/AnnouncementBanner/banners/FogoPresaleBanner.tsx
+++ packages/frontend/app/components/AnnouncementBanner/banners/FogoPresaleBanner.tsx
@@ -5,13 +5,10 @@ export default function FogoPresaleBanner() {
     const PRESALE_URL = 'https://presale.fogo.io/';
 
     return (
-        <a
-            href={PRESALE_URL}
-            target='_blank'
-            rel='noopener noreferrer'
-            className={styles.fogo_header_banner_wrapper}
-        >
-            <img src={PresaleBannerWide} alt='Fogo Presale Banner' />
-        </a>
+        <div className={styles.fogo_header_banner_wrapper}>
+            <a href={PRESALE_URL} target='_blank' rel='noopener noreferrer'>
+                <img src={PresaleBannerWide} alt='Fogo Presale Banner' />
+            </a>
+        </div>
     );
 }
