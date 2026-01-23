5748ea51b327a2aee5213ef860ae518265960e1f
Ridvan Altunel <raltunel@proven.com.tr>
2026-01-23 15:17:38 +0300

    trio gradient for mid level liqs

--- packages/frontend/app/routes/trade/liquidationsChart/LiquidationUtils.ts
+++ packages/frontend/app/routes/trade/liquidationsChart/LiquidationUtils.ts
@@ -24,12 +24,14 @@ export type LiqLevel = {
 };
 
 export const LIQ_COLOR_TRANSPARENT = 'transparent';
-export const LIQ_COLOR_BASE = '#30548377';
+export const LIQ_COLOR_BASE = '#44095f';
+export const LIQ_COLOR_MID = '#1e918e';
 export const LIQ_COLOR_TOP = '#21c879';
 export const LIQ_COLOR_YELLOW = '#FDE725';
 
 const hexToRgb = (hex: string): [number, number, number] => {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    const result =
+        /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})?$/i.exec(hex);
     return result
         ? [
               parseInt(result[1], 16),
@@ -45,8 +47,24 @@ const rgbToHex = (r: number, g: number, b: number): string => {
 
 export const interpolateLiqColor = (t: number): string => {
     const [r1, g1, b1] = hexToRgb(LIQ_COLOR_BASE);
-    const [r2, g2, b2] = hexToRgb(LIQ_COLOR_TOP);
-    return rgbToHex(r1 + (r2 - r1) * t, g1 + (g2 - g1) * t, b1 + (b2 - b1) * t);
+    const [r2, g2, b2] = hexToRgb(LIQ_COLOR_MID);
+    const [r3, g3, b3] = hexToRgb(LIQ_COLOR_TOP);
+
+    if (t <= 0.5) {
+        const localT = t * 2; // normalize 0-0.5 to 0-1
+        return rgbToHex(
+            r1 + (r2 - r1) * localT,
+            g1 + (g2 - g1) * localT,
+            b1 + (b2 - b1) * localT,
+        );
+    } else {
+        const localT = (t - 0.5) * 2; // normalize 0.5-1 to 0-1
+        return rgbToHex(
+            r2 + (r3 - r2) * localT,
+            g2 + (g3 - g2) * localT,
+            b2 + (b3 - b2) * localT,
+        );
+    }
 };
 
 export const getLiqColorForValue = (
@@ -71,7 +89,6 @@ export const parseLiqLevelRaw = (
     cumulativeSz: number,
     maxSz: number,
     totalSz: number,
-    // cumulativeRatioCoef : number = 1,
 ): LiqLevel => {
     const sz = level[1] / 1e8;
     return {
